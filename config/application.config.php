7d0ca67b14ae14bb8812d347865b668cb955a7af
Alexander Sergeychik <alexander.sergeychik@gmail.com>
2013-05-08 07:06:16 -0430

    Application config fix to make application boot through boot.php

--- config/application.config.php
+++ config/application.config.php
@@ -1,19 +1,19 @@
 <?php
 return array(
 	'modules' => array(
-		'ZfcBase',
-		'ZfcUser',
-		'Application',
-		'Admin',
+		//'ZfcBase',
+		//'ZfcUser',
+		//'Application',
+		//'Admin',
 	),
 	'module_listener_options' => array(
 		'config_glob_paths'	=> array(
 			'config/autoload/{,*.}{global,local}.php',
 		),
 		'module_paths' => array(
-			'./module',
-		    './vendor/zf-commons',
-			'./vendor/doctrine',
+			'./modules',
+			//'./vendor/zf-commons',
+			//'./vendor/doctrine',
 		),
 	),
 );
